b630c18a39e1207a72d5b07ef10658f5ef4914bc
Rokas Cvirka <rokas42@gmail.com>
2022-09-15 16:20:35 +0300

    Add project files.

---
+++ Tetris/Block.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationstate;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationstate])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        public void RotateCW()
+        {
+            rotationstate = (rotationstate + 1) % Tiles.Length;
+        }
+        public void RotateCCW()
+        {
+            if (rotationstate == 0)
+            {
+                rotationstate = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationstate--;
+            }
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationstate = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
+    }
+
+}
---
+++ Tetris/TBlock.cs
@@ -0,0 +1,18 @@
+﻿namespace Tetris
+{
+    public class TBlock : Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+         new Position[]{ new(0,1), new(1,0), new(1,1), new(1,2) },
+         new Position[]{ new(0,1), new(1,1), new(1,2), new(2,1) },
+         new Position[]{ new(1,0), new(1,1), new(1,2), new(2,1) },
+         new Position[]{ new(0,1), new(1,0), new(1,1), new(2,1) }
+        };
+        public override int Id => 6;
+
+        protected override Position[][] Tiles => tiles;
+
+        protected override Position StartOffset => new Position(0, 3);
+    }
+}
